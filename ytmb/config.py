493045e5e8233436b8773227a86e399305aff497
George Holt <georgeholt77@googlemail.com>
2025-03-30 17:22:35 +0100

    Add OATH_JSON, CLIENT_ID and CLIENT_SECRET to config.py and .env.example

--- ytmb/config.py
+++ ytmb/config.py
@@ -5,3 +5,21 @@ try:
 except KeyError:
     print("DB_URI environment variable not set. Aborting.")
     exit(1)
+
+try:
+    OATH_JSON = os.environ["OATH_JSON"]
+except KeyError:
+    print("OATH_JSON environment variable not set. Aborting.")
+    exit(1)
+
+try:
+    CLIENT_ID = os.environ["CLIENT_ID"]
+except KeyError:
+    print("CLIENT_ID environment variable not set. Aborting.")
+    exit(1)
+
+try:
+    CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+except KeyError:
+    print("CLIENT_SECRET environment variable not set. Aborting.")
+    exit(1)
