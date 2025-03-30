a04393330e2bdcec002aa36c48a259eb51fa101e
George Holt <georgeholt77@googlemail.com>
2025-03-30 17:25:24 +0100

    Add store_playlists function to db.py

--- ytmb/db.py
+++ ytmb/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker
-from models import Base
+from models import Base, Playlist
 from config import DB_URI
 
 engine = create_engine(DB_URI)
@@ -9,3 +10,22 @@ Session = sessionmaker(bind=engine)
 
 def initialize_database():
     Base.metadata.create_all(engine)
+
+
+def store_playlists(session, playlists_data):
+    for playlist_data in playlists_data:
+        exists = (
+            session.query(Playlist)
+            .filter_by(ytmusic_id=playlist_data["playlistId"])
+            .first()
+        )
+
+        if not exists:
+            new_playlist = Playlist(
+                ytmusic_id=playlist_data["playlistId"], title=playlist_data["title"]
+            )
+            session.add(new_playlist)
+            try:
+                session.commit()
+            except IntegrityError:
+                session.rollback()
