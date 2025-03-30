d91f5e9b3b2c981cd72d2c6ad25a611e43e54f99
George Holt <georgeholt77@googlemail.com>
2025-03-30 17:24:55 +0100

    Initialise api_client.py and implement get_all_playlists and get_playlist_tracks functions

---
+++ ytmb/api_client.py
@@ -0,0 +1,18 @@
+from ytmusicapi import YTMusic, OAuthCredentials
+from config import OATH_JSON, CLIENT_ID, CLIENT_SECRET
+
+ytmusic = YTMusic(
+    OATH_JSON,
+    oauth_credentials=OAuthCredentials(
+        client_id=CLIENT_ID, client_secret=CLIENT_SECRET
+    ),
+)
+
+
+def get_all_playlists():
+    return ytmusic.get_library_playlists(limit=None)
+
+
+def get_playlist_tracks(playlist_id):
+    playlist = ytmusic.get_playlist(playlistId=playlist_id, limit=None)
+    return playlist["tracks"]
