a2e51b37608fe6b0ca94d64b122d7535f977eb0c
George Holt <georgeholt77@googlemail.com>
2025-04-02 08:55:24 +0100

    Add storing of artists to main application flow

--- ytmb/main.py
+++ ytmb/main.py
@@ -1,11 +1,19 @@
 from tqdm import tqdm
-from api_client import get_all_playlists, get_playlist_tracks, get_all_albums
+from api_client import (
+    get_all_playlists,
+    get_playlist_tracks,
+    get_all_albums,
+    get_all_artists,
+    get_all_subscriptions,
+)
 from db import (
     Session,
     initialize_database,
     store_playlists,
     store_track_from_playlist,
     store_user_saved_album,
+    store_artist,
+    store_subscribed_artist,
 )
 
 
@@ -25,6 +33,15 @@ def main():
     all_albums = get_all_albums()
     for album in tqdm(all_albums):
         store_user_saved_album(session, album)
+
+    all_artists = get_all_artists()
+    for artist in tqdm(all_artists):
+        store_artist(session, artist)
+
+    all_subscriptions = get_all_subscriptions()
+    for subscription in tqdm(all_subscriptions):
+        if subscription["type"] == "artist":
+            store_subscribed_artist(session, subscription)
 
     session.close()
 
