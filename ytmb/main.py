5c0a69113fd0fd8888198f03f99a57bb3eed8cdf
George Holt <georgeholt77@googlemail.com>
2025-04-30 08:51:12 +0100

    Overhaul progress bars

--- ytmb/main.py
+++ ytmb/main.py
@@ -22,31 +22,38 @@ def main():
     initialize_database()
     session = Session()
 
+    print("Getting playlists")
     playlists = get_all_playlists()
 
     playlists = store_playlists(session, playlists)
 
-    for playlist in playlists:
+    print("Storing playlist tracks")
+    for playlist in tqdm(playlists):
         tracks = get_playlist_tracks(playlist["ytmusic_id"])
         store_artists_from_tracks(session, tracks)
         store_albums_from_tracks(session, tracks)
-        for i, track in enumerate(tqdm(tracks)):
+        for i, track in enumerate(tracks):
             store_track_from_playlist(session, playlist["playlist_table_id"], track, i)
 
+    print("Getting albums")
     all_albums = get_all_albums()
     for album in tqdm(all_albums):
         store_user_saved_album(session, album)
 
+    print("Getting artists")
     all_artists = get_all_artists()
     for artist in tqdm(all_artists):
         store_artist_from_artist_data(session, artist)
 
+    print("Getting subscriptions")
     all_subscriptions = get_all_subscriptions()
     for subscription in tqdm(all_subscriptions):
         if subscription["type"] == "artist":
             store_artist_from_artist_data(session, subscription, user_saved=True)
 
     session.close()
+
+    print("Done")
 
 
 if __name__ == "__main__":
